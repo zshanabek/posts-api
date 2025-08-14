bc6b88f396a9b6beecc5b0a8d43a062158786e52
Zhunisali <zshanabek@gmail.com>
2025-08-14 09:51:25 +0500

    added models Article

---
+++ Models/Article.cs
@@ -0,0 +1,9 @@
+namespace PostsBlogApi.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
